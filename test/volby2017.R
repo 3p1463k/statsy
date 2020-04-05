c80130e39416af50b55d8ea22ad3465a5e903d48
3p1463k <epsolar99@gmail.com>
2020-04-05 16:19:22 +0000

    prepare data

--- test/volby2017.R
+++ test/volby2017.R
@@ -1,14 +1,26 @@
 library(RCzechia)
 
-cz <- RCzechia::okresy("low")
+cz <- RCzechia::kraje("low")
 
 
-volby2017 <- read_csv("/cloud/project/data/csv/volby_do_ps_2017.csv")
+volby2017 <- read.csv("/cloud/project/data/csv/volby_do_ps_2017.csv", 
+                      stringsAsFactors = FALSE, check.names=FALSE)
+drops <- "CelkemČR"
+v1 <- volby2017[ , !(names(volby2017) %in% drops)]
+v2 <- as.data.frame(t(v1), stringsAsFactors=FALSE)
+names(v2) <- v1[,1]
+v2 <- v2 %>% slice(2:nrow(v2))
+v2 <- data.frame(lapply(v2, as.numeric), stringsAsFactors=FALSE)
+v2 <-  v2 %>% mutate(NAZ_CZNUTS3= names(v1)[2:ncol(v1)])
 
 
-ano <- volby2017 %>% filter(Name=="ANO")
 
-ggplot(data = volby2017) +
+
+ano <- v1 %>% filter(Name=="ANO")
+ano1 <- ano %>% as.data.frame(t(ano))
+
+
+ggplot(data = ano) +
   geom_sf(aes(fill=CelkemČR)) +
   scale_fill_continuous(high = "#DC143C", low = "goldenrod")+
   geom_sf_text(aes(label=Pocet), size=5)+
