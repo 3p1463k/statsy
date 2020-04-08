138102677d20d5e72ca466ea27efd9dbd90bc99d
3p1463k <epsolar99@gmail.com>
2020-04-08 07:19:32 +0000

    adding EU

--- doc/proj/test/ecdcov19.R
+++ doc/proj/test/ecdcov19.R
@@ -15,7 +15,6 @@ ecdc$year <- as.numeric(ecdc$year)
 ecdc$popData2018 <- as.numeric(ecdc$popData2018)
 ecdc$cases <- as.numeric(ecdc$cases)
 ecdc$dateRep <- as.Date(ecdc$dateRep)
-#ecd <- as.data.frame(ecdc)
 
 ecd <- ecdc %>% filter(deaths>0 & cases>0, popData2018>35000) %>%  mutate(casespm = cases/popData2018*1e+06, deathspm=deaths/popData2018*1e+06)
 
@@ -35,6 +34,7 @@ for (country in unique(ecd$countriesAndTerritories)) {
 }
 
 
+#DAILY INCREASE
 ggplot(ecd %>% filter(dateRep=="05-04-2020") , aes(cases, deaths, label=countriesAndTerritories)) + 
   geom_point(aes(col=countryterritoryCode),shape = 21,fill = "red", size = 5, stroke = 5, alpha = 1/2)+
   scale_x_log10(breaks = c(100, 1000, 5000, 25000))+
@@ -73,15 +73,15 @@ ggplot(ecd %>% filter(dateRep=="05-04-2020") , aes(cases, deaths, label=countrie
 
 
 
-#PER MILLION PEOPLE
+#PER MILLION PEOPLE INFECTED TOTAL
 
-ggplot(ecdsum , aes(cpm, dpm, label=Country)) + 
+ggplot(ecdsum %>% filter(dpm>0.05) , aes(cpm, dpm, label=Country)) + 
   geom_point(aes(col=Country),shape = 21,fill = "red", size = 5, stroke = 5, alpha = 1/2)+
   scale_x_log10(breaks = c(1, 5, 10, 20, 50,100, 500, 1000, 2000,5000))+
   scale_y_log10(breaks= c(0.1, 1, 5, 10, 20, 50, 100, 350))+
   geom_text_repel(size=4)+
-  xlab("Tested Positive")+
-  ylab("Deaths")+
+  xlab("Tested Positive per million")+
+  ylab("Deaths per million")+
   theme_minimal()+
   theme(panel.background = element_rect(fill ="#fffefc", color = "black"),
         plot.background = element_rect(fill = "#fffefc", color="black"),
@@ -105,9 +105,20 @@ ggplot(ecdsum , aes(cpm, dpm, label=Country)) +
         plot.margin = unit(c(15, 25, 5, 5), "pt"),
         plot.subtitle = element_text(size = 15)
   )+
-  labs(title = "Daily nCov19", subtitle = date(), caption="Source : European Centre for Disease Prevention and Control")+
+  labs(title = "Daily nCov19 World Total", subtitle = date(), caption="Source : European Centre for Disease Prevention and Control")+
   annotate("text", x = 2500  , y = 0.01, label = "Datastak",
            hjust=0.8, vjust=0.2, col="black", cex=6, alpha = 0.05)
+
+
+#EUROPE
+
+# Member States of the European Union
+europeanUnion <- c("Austria","Belgium","Bulgaria","Croatia","Cyprus",
+                   "Czech Rep.","Denmark","Estonia","Finland","France",
+                   "Germany","Greece","Hungary","Ireland","Italy","Latvia",
+                   "Lithuania","Luxembourg","Malta","Netherlands","Poland",
+                   "Portugal","Romania","Slovakia","Slovenia","Spain",
+                   "Sweden","United Kingdom")
 
 
 
